7dcdeead6702f89750a1b6e28dcf9ddd313a8113
zhaiy <zhaiy@rose-hulman.edu>
2017-11-27 17:04:21 -0500

    finished

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Zhai Yihu.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,32 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+dave = rg.SimpleTurtle('turtle')
+dave.pen = rg.Pen('red', 3)
+dave.speed = 10
+size1 = 200
+matt = rg.SimpleTurtle('turtle')
+matt.pen = rg.Pen('blue', 3)
+matt.speed = 10
+matt.left(45)
+size2 = 282
+for k in range(10):
+    dave.draw_circle(size1)
+    dave.pen_up()
+    dave.left(90)
+    dave.forward(10)
+    dave.right(90)
+    dave.pen_down()
+    size1 = size1 - 10
+    matt.draw_square(size2)
+    matt.pen_up()
+    matt.left(45)
+    matt.forward(10)
+    matt.right(45)
+    matt.pen_down()
+    size2 = size2 - 14
+
+window.close_on_mouse_click()
